f6f07f5ff1ea3cbe49f7772253e3792b67907ae3
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2021-10-22 18:06:13 +0500

    'changes'

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="login-form-bg h-100 w-100">
+    <div class="container h-100">
+        <div class="row justify-content-center h-100">
+            <div class="col-xl-6">
+                <div class="form-input-content">
+                    <div class="card login-form mb-0">
+                        <div class="card-body pt-5">
+                            <a class="text-center" href="index.html"> <h4>Rosella</h4></a>
+
+                            <form class="mt-5 mb-5 login-input" method="POST" action="{{ route('login') }}">
+                             @csrf
+                                <div class="form-group">
+                                    <input type="email" id="email" placeholder="Enter email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input  type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter password" >
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <button class="btn login-form__btn submit w-100">Sign In</button>
+                            </form>
+                            <div class="row">
+                                {{-- <div class="col-6">
+                                    <p class="login-form__footer">Dont have account? <a href="page-register.html" class="text-primary">Sign Up</a> now</p>
+                                </div> --}}
+                                <div class="col-6">
+                                    @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
