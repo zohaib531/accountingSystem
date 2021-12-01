d7537bb9ab93ab72cc8c59a47f67e2949098d79b
MuhammadWaseem961 <76106518+waseemewdtech@users.noreply.github.com>
2021-12-01 10:47:31 +0500

    1-21-21 partial changes

--- app/Http/Controllers/TrialBalanceController.php
+++ app/Http/Controllers/TrialBalanceController.php
@@ -21,6 +21,8 @@ class TrialBalanceController extends Controller
         $validations = Validator::make($request->all(), ['sub_account'=>'required','start_date' => 'required','end_date' => 'required|after_or_equal:start_date']);
         if ($validations->fails()) { return response()->json(['success' => false, 'message' => $validations->errors()]);}
         $vouchers = VoucherDetail::where('sub_account_id',$request->sub_account)->whereBetween('date',[$request->start_date, $request->end_date])->orderBy('date')->get();
-        return response()->json(['success' => true, 'html' => view('admin.reports.trial_balance.get_data',compact('vouchers'))->render()]);
+        $totalDebit = VoucherDetail::where('sub_account_id',$request->sub_account)->whereBetween('date',[$request->start_date, $request->end_date])->sum('debit_amount');
+        $totalCredit = VoucherDetail::where('sub_account_id',$request->sub_account)->whereBetween('date',[$request->start_date, $request->end_date])->sum('credit_amount');
+        return response()->json(['success' => true, 'html' => view('admin.reports.trial_balance.get_data',compact('vouchers','totalDebit','totalCredit'))->render()]);
     }
 }
