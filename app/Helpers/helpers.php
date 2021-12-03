cc26e89308c86da7e716fb114e04b946508d571a
MuhammadWaseem961 <76106518+waseemewdtech@users.noreply.github.com>
2021-12-03 14:45:11 +0500

    3-12-21 changes

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,42 @@
+<?php
+    use App\SubAccount;
+    use App\VoucherDetail;
+
+    if(!function_exists('getOpeningBalance')){
+        function getOpeningBalance($subAccountID,$date,$specific=false,$id=0){
+            $subAccount = SubAccount::where('id',$subAccountID)->first();
+            $openingBalance = $subAccount->opening_balance;
+            $transactionType = $subAccount->transaction_type;
+            $vouchers = $specific?VoucherDetail::where('id','!=',$id)->where('sub_account_id',$subAccount->id)->whereDate('date', '<=',$date)->get():VoucherDetail::where('sub_account_id',$subAccount->id)->whereDate('date', '<=',$date)->get();
+            if($vouchers->count() > 0){
+                foreach($vouchers as $key=>$detail){
+                    $str = $detail->entry_type."_amount";
+                    if($transactionType=="debit" && $detail->entry_type=="debit"){
+                        $openingBalance = $openingBalance + $detail->$str;
+                        $transactionType = "debit";
+                    } else if($transactionType=="credit" && $detail->entry_type=="debit"){
+                        if($openingBalance >= $detail->$str){
+                            $openingBalance = $openingBalance - $detail->$str;
+                            $transactionType = "credit";
+                        }else if($openingBalance < $detail->$str){
+                            $openingBalance = $detail->$str - $openingBalance;
+                            $transactionType = "debit";
+                        }
+                    } else if($transactionType=="credit" && $detail->entry_type=="credit"){
+                        $openingBalance = $openingBalance + $detail->$str;
+                        $transactionType = "credit";
+                    } else if($transactionType=="debit" && $detail->entry_type=="credit"){
+                        if($openingBalance >= $detail->$str){
+                            $openingBalance = $openingBalance - $detail->$str;
+                            $transactionType = "debit";
+                        }else if($openingBalance < $detail->$str){
+                            $openingBalance = $detail->$str - $openingBalance;
+                            $transactionType = "credit";
+                        }
+                    }
+                }
+            }
+
+            return ["opening_balance"=>$openingBalance,"opening_balance_type"=>$transactionType];
+        }    
+    }
--- app/Http/Controllers/SalePurchaseVoucherController.php
+++ app/Http/Controllers/SalePurchaseVoucherController.php
@@ -146,9 +146,8 @@ class SalePurchaseVoucherController extends Controller
             $check = true;   
         }
 
-        $openingBalance = isset($request->suspense_account)?VoucherDetail::where('sub_account_id',$request->suspense_account)->orderBy('id','desc')->first()!=null?VoucherDetail::where('sub_account_id',$request->suspense_account)->orderBy('id','desc')->first()->remaining_balance : SubAccount::where('id',$request->suspense_account)->first()->opening_balance:0;
-        $transactionType = isset($request->suspense_account)?VoucherDetail::where('sub_account_id',$request->suspense_account)->orderBy('id','desc')->first()!=null?VoucherDetail::where('sub_account_id',$request->suspense_account)->orderBy('id','desc')->first()->remaining_balance_type : SubAccount::where('id',$request->suspense_account)->first()->transaction_type:"";
-        
+        $openingBalance = isset($request->suspense_account)? getOpeningBalance($request->suspense_account,$request->suspense_date,false,0)["opening_balance"]:0;
+        $transactionType = isset($request->suspense_account)? getOpeningBalance($request->suspense_account,$request->suspense_date,false,0)["opening_balance_type"]:'';
         if($transactionType=="debit" && $request->suspense_entry=="debit"){
             $remainingBalance = $openingBalance + $request->suspense_amount;
             $remainingBalanceType = "debit";
@@ -203,8 +202,9 @@ class SalePurchaseVoucherController extends Controller
                 }else{
                     $VoucherDetail = new VoucherDetail();
                 }
-                $openingBalance = VoucherDetail::where('sub_account_id',$request->debit_accounts[$key])->orderBy('id','desc')->first()!=null?VoucherDetail::where('sub_account_id',$request->debit_accounts[$key])->orderBy('id','desc')->first()->remaining_balance : SubAccount::where('id',$request->debit_accounts[$key])->first()->opening_balance;
-                $transactionType = VoucherDetail::where('sub_account_id',$request->debit_accounts[$key])->orderBy('id','desc')->first()!=null?VoucherDetail::where('sub_account_id',$request->debit_accounts[$key])->orderBy('id','desc')->first()->remaining_balance_type : SubAccount::where('id',$request->debit_accounts[$key])->first()->transaction_type;
+
+                $openingBalance =  getOpeningBalance($request->debit_accounts[$key],$request->debit_dates[$key],false,0)["opening_balance"];
+                $transactionType = getOpeningBalance($request->debit_accounts[$key],$request->debit_dates[$key],false,0)["opening_balance_type"];
                 if($transactionType=="debit"){
                     $remainingBalance = $openingBalance + $request->debit_amounts[$key];
                     $remainingBalanceType = "debit";
@@ -245,8 +245,8 @@ class SalePurchaseVoucherController extends Controller
                     $VoucherDetail = new VoucherDetail();
                 }
 
-                $openingBalance = VoucherDetail::where('sub_account_id',$request->credit_accounts[$key])->orderBy('id','desc')->first()!=null?VoucherDetail::where('sub_account_id',$request->credit_accounts[$key])->orderBy('id','desc')->first()->remaining_balance : SubAccount::where('id',$request->credit_accounts[$key])->first()->opening_balance;
-                $transactionType = VoucherDetail::where('sub_account_id',$request->credit_accounts[$key])->orderBy('id','desc')->first()!=null?VoucherDetail::where('sub_account_id',$request->credit_accounts[$key])->orderBy('id','desc')->first()->remaining_balance_type : SubAccount::where('id',$request->credit_accounts[$key])->first()->transaction_type;
+                $openingBalance = getOpeningBalance($request->credit_accounts[$key],$request->credit_dates[$key],false,0)["opening_balance"];
+                $transactionType = getOpeningBalance($request->credit_accounts[$key],$request->credit_dates[$key],false,0)["opening_balance_type"];
                 if($transactionType=="credit"){
                     $remainingBalance = $openingBalance + $request->credit_amounts[$key];
                     $remainingBalanceType = "credit";
@@ -329,5 +329,43 @@ class SalePurchaseVoucherController extends Controller
             }
         }
         return $messages;
+    }
+
+    // get opening balance of specific sub account with date
+    public function getOpeningBalance($subAccountID,$date,$specific=false,$id=0){
+        $subAccount = SubAccount::where('id',$subAccountID)->first();
+        $openingBalance = $subAccount->opening_balance;
+        $transactionType = $subAccount->transaction_type;
+        $vouchers = $specific?VoucherDetail::where('id','!=',$id)->where('sub_account_id',$subAccount->id)->whereDate('date', '<=',$date)->get():VoucherDetail::where('sub_account_id',$subAccount->id)->whereDate('date', '<=',$date)->get();
+        if($vouchers->count() > 0){
+            foreach($vouchers as $key=>$detail){
+                $str = $detail->entry_type."_amount";
+                if($transactionType=="debit" && $detail->entry_type=="debit"){
+                    $openingBalance = $openingBalance + $detail->$str;
+                    $transactionType = "debit";
+                } else if($transactionType=="credit" && $detail->entry_type=="debit"){
+                    if($openingBalance >= $detail->$str){
+                        $openingBalance = $openingBalance - $detail->$str;
+                        $transactionType = "credit";
+                    }else if($openingBalance < $detail->$str){
+                        $openingBalance = $detail->$str - $openingBalance;
+                        $transactionType = "debit";
+                    }
+                } else if($transactionType=="credit" && $detail->entry_type=="credit"){
+                    $openingBalance = $openingBalance + $detail->$str;
+                    $transactionType = "credit";
+                } else if($transactionType=="debit" && $detail->entry_type=="credit"){
+                    if($openingBalance >= $detail->$str){
+                        $openingBalance = $openingBalance - $detail->$str;
+                        $transactionType = "debit";
+                    }else if($openingBalance < $detail->$str){
+                        $openingBalance = $detail->$str - $openingBalance;
+                        $transactionType = "credit";
+                    }
+                }
+            }
+        }
+
+        return ["opening_balance"=>$openingBalance,"opening_balance_type"=>$transactionType];
     }
 }
