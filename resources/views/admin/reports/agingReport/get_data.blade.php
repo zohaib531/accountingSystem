ddf8edd9e90b3149d3d292966f1bf885b95190b7
MuhammadWaseem961 <76106518+waseemewdtech@users.noreply.github.com>
2021-12-03 17:09:27 +0500

    Update get_data.blade.php

--- resources/views/admin/reports/agingReport/get_data.blade.php
+++ resources/views/admin/reports/agingReport/get_data.blade.php
@@ -18,38 +18,53 @@
         @endphp
         @if(isset($vouchers) && $vouchers->count()>0)
             @foreach($vouchers as $key=>$detail)
-                @if($loop->first)
-                    @php $openingBalance = $detail->remaining_balance; @endphp;
-                @endif
-                @php
-                    // $str = $detail->entry_type."_amount";
 
-                    // if($openingBalance > 0 ){
-                    //     $openingBalance = $openingBalance - $detail->$str;
-                    // }
+                @if($loop->first)
+                    @php  
+                        $openingBalance = getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id)["opening_balance"];
+                        $entryType = getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id)["opening_balance_type"];
+                    @endphp
+                    <tr>
+                        <td>{{date('d-m-Y',strtotime($detail->date))}}</td>
+                        <td></td>
+                        <td>Opening Balance</td>
+                        <td colspan="2"></td>
+                        <td>{{ getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id)["opening_balance"] }}</td>
+                        <td>{{ getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id)["opening_balance_type"] }}</td>
+                    </tr>
+                @endif
+                {{-- @if($loop->first)
+                    @php $openingBalance = $detail->remaining_balance; @endphp;
+                @endif --}}
+                @php
+                    $str = $detail->entry_type."_amount";
+                    if($openingBalance > 0 ){
+                        $openingBalance = $openingBalance - $detail->$str;
+                    }
 
 
                     $to = \Carbon\Carbon::createFromFormat('Y-m-d', $detail->date);
                     $from = \Carbon\Carbon::createFromFormat('Y-m-d', date('Y-m-d'));
                     $diff_in_days = $to->diffInDays($from);
-                    @endphp
-
-                <tr>
-                    <td>{{date('d-m-Y',strtotime($detail->date))}}</td>
-                    <td>{{$diff_in_days}}</td>
-                    <td>{{$detail->product_narration}} @if($detail->quantity!=0 && $detail->rate!=0)  (<span style="font-weight:bold;">{{$detail->quantity}} x {{$detail->rate}}</span>) @endif</td>
-                    <td>{{ $detail->debit_amount!=0? number_format($detail->debit_amount) :"" }}</td>
-                    <td>{{ $detail->credit_amount!=0? number_format($detail->credit_amount):"" }}</td>
-                    {{-- <td>{{ number_format($openingBalance)}}</td> --}}
-                    <td>{{ number_format($detail->remaining_balance)}}</td>
-                    <td>{{$detail->remaining_balance_type}}</td>
-                </tr>
-                @if($loop->last)
+                @endphp
+                @if($openingBalance > 0 )
+                    <tr>
+                        <td>{{date('d-m-Y',strtotime($detail->date))}}</td>
+                        <td>{{$diff_in_days}}</td>
+                        <td>{{$detail->product_narration}} @if($detail->quantity!=0 && $detail->rate!=0)  (<span style="font-weight:bold;">{{$detail->quantity}} x {{$detail->rate}}</span>) @endif</td>
+                        <td>{{ $detail->debit_amount!=0? number_format($detail->debit_amount) :"" }}</td>
+                        <td>{{ $detail->credit_amount!=0? number_format($detail->credit_amount):"" }}</td>
+                        {{-- <td>{{ number_format($openingBalance)}}</td> --}}
+                        <td>{{ number_format($openingBalance)}}</td>
+                        <td>{{$entryType}}</td>
+                    </tr>
+                @endif
+                {{-- @if($loop->last)
                     @php 
                         $openingBalance = $detail->remaining_balance; 
                         $entryType = $detail->remaining_balance_type; 
                     @endphp
-                @endif
+                @endif --}}
             @endforeach
 
         @else
