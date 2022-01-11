615ad6c10d853adb30dae2b8c81200ec58044ef8
MuhammadWaseem961 <76106518+waseemewdtech@users.noreply.github.com>
2022-01-11 18:11:49 +0500

    11-01-22 partial changes

--- app/Http/Controllers/AgingReportController.php
+++ app/Http/Controllers/AgingReportController.php
@@ -21,36 +21,64 @@ class AgingReportController extends Controller
 
      // get data for trial balance
      public function entriesBetweenDates(Request $request){
-        $start_date = '';
-        $end_date = '';
+
         // $validations = Validator::make($request->all(), ['account'=>'required','sub_account'=>'required','start_date' => 'required_with_all:end_date','end_date' => 'required_with_all:start_date']);
         $validations = Validator::make($request->all(), ['account'=>'required','sub_account'=>'required','start_date' => 'required','end_date' => 'required']);
         if ($validations->fails()) { return response()->json(['success' => false, 'message' => $validations->errors()]);}
 
-        if( $request->start_date !== ''){
-            $start_date = Carbon::createFromFormat('d / m / y',  $request->start_date)->format('y-m-d');
-        }
+        
+        $start_date = Carbon::createFromFormat('d / m / y',  $request->start_date)->format('y-m-d');
+        $end_date = Carbon::createFromFormat('d / m / y', $request->end_date)->format('y-m-d');
 
-        if($request->end_date !== ''){
-            $end_date = Carbon::createFromFormat('d / m / y', $request->end_date)->format('y-m-d');
-        }
+        if($request->account!="all" && $request->sub_account=="all"){
+            $account = Account::where('id',$request->account)->first();
+            $subAccounts = $account->get_sub_accounts()->pluck('id');
+            $vouchers = VoucherDetail::whereIn('sub_account_id',$subAccounts)->whereBetween('date',[$start_date, $end_date])->get()->groupBy("sub_account_id");
 
-        if($request->sub_account!="all" && $request->sub_account!=""){
-            // $vouchers = VoucherDetail::where("sub_account_id",$request->sub_account)->whereBetween('date',[Carbon::createFromFormat('d / m / y', $request->start_date)->format('y-m-d'), Carbon::createFromFormat('d / m / y', $request->end_date)->format('y-m-d')]);
-            $vouchers = VoucherDetail::where("sub_account_id",$request->sub_account)->whereBetween('date',[$start_date, $end_date]);
+        }else if($request->account!="all" && $request->sub_account!="all"){
+            $vouchers = VoucherDetail::where('sub_account_id',$request->sub_account)->whereBetween('date',[$start_date, $end_date])->get()->groupBy("sub_account_id");
 
         }else{
-
-            $vouchers = VoucherDetail::whereBetween('date',[$start_date, $end_date]);
-
+            $vouchers = VoucherDetail::whereBetween('date',[$start_date, $end_date])->get()->groupBy("sub_account_id");
         }
 
-        $vouchers =$vouchers->get();
-        // return $vouchers;
-        $subAccount = getOpeningBalance($request->sub_account,date("y-m-d"),false,0);
-        $totalDebit = VoucherDetail::where('sub_account_id',$request->sub_account)->where('entry_type', $subAccount["opening_balance_type"])->whereDate('date', '<=',date("Y-m-d"))->sum('debit_amount');
-        $totalCredit = VoucherDetail::where('sub_account_id',$request->sub_account)->where('entry_type', $subAccount["opening_balance_type"])->whereDate('date', '<=',date("Y-m-d"))->sum('credit_amount');
-        return response()->json(['success' => true, 'html' => view('admin.reports.agingReport.get_data',compact('vouchers','totalDebit','totalCredit','subAccount','start_date','end_date'))->render()]);
+        $openingBalance = 0;
+        $debitBalance = 0;
+        $creditBalance = 0;
+        $entryType = '';
+
+        if($vouchers->count()>0){
+            foreach($vouchers as $key=>$subAccountEntries){
+                $subAccount = SubAccount::where('id',$key)->first();
+                if($subAccountEntries->count()>0){
+                    foreach ($subAccountEntries as $key => $singleEntry) {
+                        if($key==0){
+                            $getOpeningBalanceResponse = getOpeningBalance($singleEntry->sub_account_id,$singleEntry->date,true,$singleEntry->id);
+                            $openingBalance = $getOpeningBalanceResponse["opening_balance"];
+                            $entryType = $getOpeningBalanceResponse["opening_balance_type"];
+                        }
+
+                        $str = $detail->entry_type."_amount";
+                            if($openingBalance > 0 ){
+                                $detail->entry_type=="debit"?$debitBalance += $detail->$str:$creditBalance += $detail->$str;
+                                $openingBalance = $openingBalance - $detail->$str;
+                            }else{
+                                $entryType = $entryType=="debit"? "credit":"debit";
+                            }
+                            $to = Carbon::createFromFormat('Y-m-d', $detail->date);
+                            $from = Carbon::createFromFormat('Y-m-d', date('Y-m-d'));
+                            $diff_in_days = $to->diffInDays($from);
+                    }
+                }
+            }
+        }else{
+            return "so";
+        }
+        return $vouchers;
+        // $subAccount = getOpeningBalance($request->sub_account,date("y-m-d"),false,0);
+        // $totalDebit = VoucherDetail::where('sub_account_id',$request->sub_account)->where('entry_type', $subAccount["opening_balance_type"])->whereDate('date', '<=',date("Y-m-d"))->sum('debit_amount');
+        // $totalCredit = VoucherDetail::where('sub_account_id',$request->sub_account)->where('entry_type', $subAccount["opening_balance_type"])->whereDate('date', '<=',date("Y-m-d"))->sum('credit_amount');
+        return response()->json(['success' => true, 'html' => view('admin.reports.agingReport.get_data',compact('vouchers','start_date','end_date'))->render()]);
 
     }
 
---
+++ resources/views/admin/reports/agingReport/get_data110122341.blade.php
@@ -0,0 +1,93 @@
+<table class="table table-striped table-bordered zero-configuration">
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Days</th>
+            <th>Narration/Details</th>
+            <th>Debit</th>
+            <th>Credit</th>
+            <th>Balance</th>
+            <th>Nature (Debit/Credit)</th>
+            {{-- <th class="text-right">Action</th> --}}
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            $openingBalance = 0;
+            $debitBalance = 0;
+            $creditBalance = 0;
+            $entryType = '';
+        @endphp
+        @if(isset($vouchers) && $vouchers->count()>0)
+            @foreach($vouchers as $key=>$detail)
+
+                @if($loop->first)
+                    @php
+                        $getOpeningBalanceResponse = getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id);
+                        $openingBalance = $getOpeningBalanceResponse["opening_balance"];
+                        $entryType = $getOpeningBalanceResponse["opening_balance_type"];
+                    @endphp
+                    <tr>
+                        <td>{{date('d/m/y',strtotime($detail->date))}}</td>
+                        <td></td>
+                        <td>Opening Balance</td>
+                        <td colspan="2"></td>
+                        <td>{{ number_format(getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id)["opening_balance"] , 2) }}</td>
+                        <td>{{ getOpeningBalance($detail->sub_account_id,$detail->date,true,$detail->id)["opening_balance_type"] }}</td>
+                    </tr>
+                @endif
+                {{-- @if($loop->first)
+                    @php $openingBalance = $detail->remaining_balance; @endphp;
+                @endif --}}
+                @php
+                    $str = $detail->entry_type."_amount";
+                    if($openingBalance > 0 ){
+                        $detail->entry_type=="debit"?$debitBalance += $detail->$str:$creditBalance += $detail->$str;
+                        $openingBalance = $openingBalance - $detail->$str;
+                    }else{
+                        $entryType = $entryType=="debit"? "credit":"debit";
+                    }
+                    $to = \Carbon\Carbon::createFromFormat('Y-m-d', $detail->date);
+                    $from = \Carbon\Carbon::createFromFormat('Y-m-d', date('Y-m-d'));
+                    $diff_in_days = $to->diffInDays($from);
+                @endphp
+
+                <tr>
+                    <td>{{date('d/m/y',strtotime($detail->date))}}</td>
+                    <td>{{$diff_in_days}}</td>
+                    <td>{{$detail->product_narration}} @if($detail->quantity!=0 && $detail->rate!=0)  (<span style="font-weight:bold;">{{$detail->quantity}} x {{$detail->rate}}</span>) @endif</td>
+                    <td>{{ $detail->debit_amount!=0? number_format($detail->debit_amount , 2) :"" }}</td>
+                    <td>{{ $detail->credit_amount!=0? number_format($detail->credit_amount , 2):"" }}</td>
+                    {{-- <td>{{ number_format($openingBalance)}}</td> --}}
+                    <td>{{ number_format($openingBalance , 2)}}</td>
+                    <td>{{$entryType}}</td>
+                </tr>
+                @if($openingBalance < 0)
+                    @php break; @endphp
+                @endif
+                {{-- @if($loop->last)
+                    @php
+                        $openingBalance = $detail->remaining_balance;
+                        $entryType = $detail->remaining_balance_type;
+                    @endphp
+                @endif --}}
+            @endforeach
+
+        @else
+            <tr>
+                <td colspan="8" class="text-center">No records found</td>
+            </tr>
+        @endif
+    </tbody>
+    @if(isset($vouchers) && $vouchers->count()>0)
+        <tfoot>
+            <tr>
+                <td colspan="3"><h5 class="text-center">Total</h5></td>
+                <td>{{number_format($debitBalance , 2)}}</td>
+                <td>{{number_format($creditBalance , 2)}}</td>
+                <td>{{number_format($openingBalance , 2)}}</td>
+                <td>{{$entryType}}</td>
+            </tr>
+        </tfoot>
+    @endif
+</table>
